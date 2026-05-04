3c277757a0982204527e0d049b514e928b846826
Brandon Weathers <brandon.r.weathers@gmail.com>
2026-05-04 02:57:28 -0400

    strange array logic cuases indexing problem

--- MainScreen.java
+++ MainScreen.java
@@ -6,6 +6,7 @@
 
 import java.lang.Math;
 import java.util.*;
+import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.*;
@@ -20,14 +21,18 @@ import javafx.stage.*;
 public class MainScreen extends Application {
     String currentInput = "";
 
+    // The list of instances of the MathHandler
+    int currentIndex = 0;
+
     public static void main(String[] args) {
         launch();
     }
 
     @Override
     public void start(Stage stage) {
-        // The instance of the MathHandler 
-        MathHandler M = new MathHandler();
+        MathHandler[] mathHandlerArray = new MathHandler[2];
+        mathHandlerArray[0] = new MathHandler();
+        mathHandlerArray[1] = new MathHandler();
 
         // Queue 0 label and its output
         Label queue0Label = new Label("Master queue");
@@ -38,7 +43,7 @@ public class MainScreen extends Application {
         Label queue0Output = new Label("?");
         queue0Output.setFont(new Font("Terminess Nerd Font", 40));
         queue0Output.setTextFill(Color.WHITE);
-        queue0Output.setText(returnQueuekAsString(M.mainQueue));
+        queue0Output.setText(returnQueuekAsString(mathHandlerArray[0].mainQueue));
 
         // Queue 1 label and its output
         Label queue1Label = new Label("Sub-queue one");
@@ -49,7 +54,7 @@ public class MainScreen extends Application {
         Label queue1Output = new Label("?");
         queue1Output.setFont(new Font("Terminess Nerd Font", 40));
         queue1Output.setTextFill(Color.WHITE);
-        queue1Output.setText(returnQueuekAsString(M.mainQueue));
+        queue1Output.setText(returnQueuekAsString(mathHandlerArray[1].mainQueue));
 
         GridPane mainGrid = new GridPane();
         mainGrid.setHgap(30.0);
@@ -78,21 +83,16 @@ public class MainScreen extends Application {
         Scene scene = new Scene(root, 500, 500);
 
         scene.setOnKeyPressed(e -> {
-                if(e.getCode() == KeyCode.C) {
-                    M.mainQueue.clear();
-                    currentInput = "";
-                    input.setText("\nInput: ");
-                }
 
-                if((e.getCode() == KeyCode.EQUALS && e.isShiftDown()) || (e.getCode() == KeyCode.ADD)) M.addQueue();
-                if((e.getCode() == KeyCode.MINUS) || (e.getCode() == KeyCode.SUBTRACT)) M.subQueue();
-				if((e.getCode() == KeyCode.DIGIT8 && e.isShiftDown()) || (e.getCode() == KeyCode.MULTIPLY)) M.mulQueue();
-				if((e.getCode() == KeyCode.SLASH) || (e.getCode() == KeyCode.DIVIDE)) M.divQueue();
-				if(e.getCode() == KeyCode.DIGIT6 && e.isShiftDown() && e.isControlDown()) M.expoQueueLeftToRight();
-				else if(e.getCode() == KeyCode.DIGIT6 && e.isShiftDown()) M.expoQueueTopToBottom();
-				if(e.getCode() == KeyCode.Q) M.sqrtQueue();
-				if (e.getCode() == KeyCode.M  && e.isShiftDown()) M.findMaxQueue();
-				if ((e.getCode() == KeyCode.M)) M.findMinQueue();
+                if((e.getCode() == KeyCode.EQUALS && e.isShiftDown()) || (e.getCode() == KeyCode.ADD)) mathHandlerArray[currentIndex].addQueue();
+                if((e.getCode() == KeyCode.MINUS) || (e.getCode() == KeyCode.SUBTRACT)) mathHandlerArray[currentIndex].subQueue();
+				if((e.getCode() == KeyCode.DIGIT8 && e.isShiftDown()) || (e.getCode() == KeyCode.MULTIPLY)) mathHandlerArray[currentIndex].mulQueue();
+				if((e.getCode() == KeyCode.SLASH) || (e.getCode() == KeyCode.DIVIDE)) mathHandlerArray[currentIndex].divQueue();
+				if(e.getCode() == KeyCode.DIGIT6 && e.isShiftDown() && e.isControlDown()) mathHandlerArray[currentIndex].expoQueueLeftToRight();
+				else if(e.getCode() == KeyCode.DIGIT6 && e.isShiftDown()) mathHandlerArray[currentIndex].expoQueueTopToBottom();
+				if(e.getCode() == KeyCode.Q) mathHandlerArray[currentIndex].sqrtQueue();
+				if (e.getCode() == KeyCode.M  && e.isShiftDown()) mathHandlerArray[currentIndex].findMaxQueue();
+				if ((e.getCode() == KeyCode.M)) mathHandlerArray[currentIndex].findMinQueue();
 
 				if(e.getText().matches("\\.")) { // Decimals and number
                     if(currentInput.contains(".")) {
@@ -107,20 +107,33 @@ public class MainScreen extends Application {
 
                 if(e.getCode() == KeyCode.BACK_SPACE) {
                     if(!currentInput.isEmpty()) {
-                        currentInput = currentInput.substring(0, currentInput.length() - 1);
+                        currentInput = currentInput.substring(currentIndex, currentInput.length() - 1);
                         input.setText("\nInput: " + currentInput);
                     }
                 }
 
                 if(e.getCode() == KeyCode.ENTER) { // Press (Enter) the new input
-                    M.mainQueue.add(Double.valueOf(currentInput));
+                    mathHandlerArray[currentIndex].mainQueue.add(Double.valueOf(currentInput));
                     currentInput = "";
                     input.setText("\nInput: ");
                 }
 
-                queue0Output.setText(returnQueuekAsString(M.mainQueue));
-                queue1Output.setText(returnQueuekAsString(M.mainQueue));
-        });
+                if(e.getCode() == KeyCode.C) {
+                    mathHandlerArray[currentIndex].mainQueue.clear();
+                    // currentInput = "";
+                    input.setText("\nInput: ");
+                }
+
+                queue0Output.setText(returnQueuekAsString(mathHandlerArray[0].mainQueue));
+                queue1Output.setText(returnQueuekAsString(mathHandlerArray[1].mainQueue)); // For some reason, this copies over the MathHandler from the 0th index
+
+                if(e.getCode() == KeyCode.H) {
+                    currentIndex--;
+                } else if(e.getCode() == KeyCode.L) {
+                    currentIndex++;
+                }
+            });
+
 
         // Possibly helps
         stage.setScene(scene);
